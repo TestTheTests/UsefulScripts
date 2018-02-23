9ac36327fd41d740a6b1a18d3433bb3320ced780
Kevin Freeman <freeman.k@husky.neu.edu>
2018-02-23 15:20:47 -0500

    10.25s

--- vcf2H_scan.pl
+++ vcf2H_scan.pl
@@ -42,38 +42,11 @@ unless ($vcf) {
 
 ############  Read VCF #################################################################
 
+### get file handles
 my $infh;
 unless (open($infh, '<', $vcf)){
 	die "Unable to open $vcf for reading", $!;
 }
-
-### Initialize lookup table
-my %lookupTable= ('0' => undef,		# this will change to ref allele 				
-			  	  '1' => undef,		# this will change to alt allele
-			  	  '|' => ',',
-			      '.' => 'N'		# missing data
-);
-my @allHscanLines;
-while (<$infh>){
-	if ($_ =~ /^#/){			#skip header lines
-		next;
-	}
-	my @line = split("\t",$_);
-			
-	my $position  = $line[1];
-	my $refAllele = $line[3];
-	my $altAllele = $line[4];
-	
-	my $translatedLine = vcfLine2HscanLine( { pos   => $position,  reference => $refAllele, 
-											  alt   => $altAllele, line 	 => $_ } );
-	push @allHscanLines, $translatedLine;
-	if (scalar @allHscanLines %1000 == 0){					# give user feedback
-		say "processed ",scalar @allHscanLines, " lines";
-	}
-}
-close $infh;
-
-########## Print to file ################################################################
 
 unless ($outFile){
 	$outFile = $vcf.".hscan";		# create default filename if one was not provided
@@ -83,19 +56,59 @@ unless (open($outfh, '>', $outFile)){
 	die "Can't open $outFile for writing", $!;
 }
 
-print $outfh join("\n", @allHscanLines);
+### Initialize lookup table
+my %lookupTable= ('0' => undef,		# this will change to ref allele 				
+			  	  '1' => undef,		# this will change to alt allele
+			  	  '|' => ',',
+			      '.' => 'N'		# missing data
+);
+### convert
+convertVcf($infh, $outfh);
+## give feedback and close files
 close $outfh;
+close $infh;
 say "\nCreated ", $outFile;
 
 #-----------------------------------------------------------------------
-# ($commaDelimitedString) = vcfLine2HscanLine({pos, reference, alt, line});
+# void context = convertVcf($infh, $outfh);
+#-----------------------------------------------------------------------
+# This subroutine takes an infile handle and an outfile handle. It 
+# takes the input vcf file, extracts the relevant data, and generates
+# a hscan compatible file. It depends on the _vcfLine2HscanLine subroutine
+#-----------------------------------------------------------------------
+sub convertVcf{
+	my ($inHandle, $outHandle) = @_;
+	my $i = 0;
+	while (<$infh>){
+		if ($_ =~ /^#/){			#skip header lines
+			next;
+		}
+		my @line = split("\t",$_);
+			
+		my $position  = $line[1];
+		my $refAllele = $line[3];
+		my $altAllele = $line[4];
+	
+		my $translatedLine = _vcfLine2HscanLine( { pos   	=> $position, 
+											      reference => $refAllele, 
+											  	  alt       => $altAllele, 
+											  	  line 	     => $_ } );
+		say $outHandle $translatedLine;
+		$i++;
+		if ($i % 2000 == 0) {say "Processed ".$i." variants"}; # give user feedback
+			
+	}
+}
+
+#-----------------------------------------------------------------------
+# ($commaDelimitedString) = _vcfLine2HscanLine({pos, reference, alt, line});
 #-----------------------------------------------------------------------
 # This subroutine takes a line containing genotype information and 
 # parameters to convert that information in the form of a referenced
 # hash and converts them to a line that can be used with H-Scan
 #-----------------------------------------------------------------------
 
-sub vcfLine2HscanLine {
+sub _vcfLine2HscanLine {
 	my ($refArgs) = @_;
 	my $line = $refArgs 	-> {line};
 	my $ref = $refArgs 		-> {reference};
