e68c75626e07438efcb510e9f7a51620358af295
Kevin Freeman <freeman.k@husky.neu.edu>
2018-05-18 16:13:52 -0400

    finishing script, still requires some testing

--- Conversion_Scripts/run_baypass/run_baypass.sh
+++ Conversion_Scripts/run_baypass/run_baypass.sh
@@ -8,14 +8,47 @@
 #######################################################################################
 #
 # This script combines multiple scripts to form an automated pipeline for analyzing
-# a batch of VCFs with Baypass. The script is meant to be run from a folder called
+# a batch of VCFs with Baypass. The script is meant to be run in a folder called
 # run_baypass that includes a directory named "input_files" that contains all the 
-# necessary input files. It takes no arguments.
+# necessary input files. The required scripts should be in the same parent directory 
+# as the run_baypass directory. It takes no arguments.
+# 
+#######################################################################################
+#
+# Example file tree with required scripts, input files, and directories:
+# (Note --- converted_files, baypass_results, final_results_tables, and log_files 
+# directories and files are created during execution of the script)
+# .
+# |
+# |----run_baypass
+# |		|------input_files
+# |     |  			|------xxxx_Invers_indFILT.txt
+# |		|			|------xxxx_Invers_ScanResults.txt
+# |		|			|------xxxx_Invers_VCFallFILT.vcf.gz
+# |     |-----converted_files     (created by script)
+# |  	|			|------xxxx_Invers_VCFallFILT.covar
+# |		|			|------xxxx_Invers_VCFallFILT.geno
+# |		|			|------xxxx_Invers_VCFallFILT_PRUNED.covar
+# |		|			|------xxxx_Invers_VCFallFILT_PRUNED.geno
+# |		|-----baypasss_results     (created by script)
+# |		|			|------
+# |		|			. (many files, see baypass 2.1 documnetation for more info)
+# |		|			.
+# |		|			. 
+# |		|-----log_files            (created by script)
+# |		|		    |------xxxx_baypass_err.txt
+# |		|			|------xxxx_baypass_log.txt					
+# |		|-----final_results_tables (created by script)
+# |		|			|------xxx_baypass_results.txt
+# |     |-----run_baypass.sh
+# |----vcf2baypass.pl
+# |----pruneSNPs.pl
+# |----getTableOfBaypassResults.pl					
 #
 #######################################################################################
-
+SECONDS=0
 mypath="/home/kevin/LOTTERHOS_LAB/UsefulScripts/Conversion_Scripts/run_baypass"
-ncpus=3
+ncpus=1
 start=10900
 finish=$(($start + $ncpus-1))
 echo $start $finish
@@ -27,11 +60,12 @@ function call_baypass_no_mat {
 	i=$3
     
     # convert vcf to baypass format, store npop in associative array (hash)
-    npopHash=([$i]="$(perl ../vcf2baypass.pl -vcf ./input_files/${i}${file_name} \
+    npopHash=( [$i]="$(perl ../vcf2baypass.pl -vcf ./input_files/${i}${file_name} \
     -pop ./input_files/${i}_Invers_indFILT.txt \
     -colGroup 5 -colEnv 4 -colPheno 3 -colInFinal 6 \
     -outGeno ./converted_files/${i}_Invers_VCFallFILT$out_suffix \
     -outCovar ./converted_files/${i}_Invers_VCFallFILT$out_suffix)")
+
     # run baypass
     g_baypass -npop ${npopHash[$i]} \
      -gfile ./converted_files/${i}_Invers_VCFallFILT${out_suffix}.geno \
@@ -79,6 +113,7 @@ do
 done
 wait
 # run baypass again, this time using the pruned covar matrix from the last step
+END
 cd $mypath
 gfileName="_Invers_VCFallFILT.geno"
 efileName="_Invers_VCFallFILT.covar"
@@ -87,24 +122,31 @@ omegaFile="_PRUNED_mat_omega.out"
 echo -e "\n###########Running baypass on all SNPs with pruned matrix##########################"
 for i in $(seq $start $finish)
 do
+	call_baypass_no_mat "_Invers_VCFallFILT.vcf" "" $i
 	echo -e "\n"$i
-	if [ ! -f  ./converted_files/${i}gfileName ]; then
+	if [ ! -f  ./converted_files/${i}$gfileName ]; then
     	echo "Geno file for ${i} not found"
     	continue
     fi 
-    if [ ! -f  ./converted_files/${i}efileName ]; then
+    if [ ! -f  ./converted_files/${i}$efileName ]; then
     	echo "env file for ${i} not found"
     	continue
     fi
-    if [ ! -f  ./baypass_results/${i}omegaFile ]; then
+    if [ ! -f  ./baypass_results/${i}$omegaFile ]; then
     	echo "Omega matrix file for ${i} not found"
     	continue
     fi
     g_baypass -npop ${npopHash[$i]} -gfile ./converted_files/${i}$gfileName \
-    -efile ./converted_files/${i}efileName -scalecov \
-    -omegafile ./baypass_results/${i}$omegaFile -outprefix "${i}_ALL_PRUNED_MAT"
+    -efile ./converted_files/${i}$efileName -scalecov \
+    -omegafile ./baypass_results/${i}$omegaFile -outprefix "${i}_ALL_PRUNED_MAT"\
+    >>./log_files/${i}_baypass_log.txt 2>>./log_files/${i}_baypass_err.txt &
     sleep 1m
 done
+wait 
 
 # pull out analysis results and store in a table
-    
+echo -e "\n################ Putting Results into Tables ##############################"
+cd ${mypath}/baypass_results
+perl ../../getTableOfBaypassResults.pl -start $start -finish $finish
+echo -e "\n\nDone. Analysis took $(($SECONDS / 3600))hrs $((($SECONDS / 60) % 60))min"
+exit 0
