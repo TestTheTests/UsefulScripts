3b7f5e24715ef9079dc77cbf4642772f6173a4ed
BrianFerolito <brian_ferolito@outlook.com>
2018-11-13 15:11:18 -0500

    Initial Commit
    Need to comment out code

---
+++ FerolitoScripts/TASSEL/cMatrixPlots.R
@@ -0,0 +1,273 @@
+#!/usr/bin/env Rscript
+
+CM.outfile<-file.create("~/Desktop/Lotterhos_Lab/allPlots/confusion_matrix.txt")
+fileConn<-file("~/Desktop/Lotterhos_Lab/allPlots/confusion_matrix.txt")
+CM.header<-paste("True Positive", "False Negative", "False Positive", "True Negative", "False Negative Missing Values", sep = "\t")
+writeLines(CM.header, fileConn)
+
+for (i in c(10900:10920, 10922:10925, 10927:10961)) {
+
+fileNumber<-i
+
+TP.file<-paste0("~/Desktop/Lotterhos_Lab/VCF/", fileNumber, ".domAdd.TP.txt")
+FN.file<-paste0("~/Desktop/Lotterhos_Lab/VCF/", fileNumber, ".domAdd.FN.txt")
+FP.file<-paste0("~/Desktop/Lotterhos_Lab/VCF/", fileNumber, ".domAdd.FP.txt")
+TN.file<-paste0("~/Desktop/Lotterhos_Lab/VCF/", fileNumber, ".domAdd.TN.txt")
+FN.missing.file<-paste0("~/Desktop/Lotterhos_Lab/VCF/", fileNumber, ".domAddMissing.FN.txt")
+
+
+TP.table<-read.table(TP.file, header = TRUE)
+FN.table<-read.table(FN.file, header = TRUE)
+FP.table<-read.table(FP.file, header = TRUE)
+TN.table<-read.table(TN.file, header = TRUE)
+FN.missing.table<-read.table(FN.missing.file, header = TRUE)
+
+TN.table$expectedAdditive<-0
+FN.missing.table$expectedAdditive<-0
+
+#Path to write plots
+
+path<-paste0("~/Desktop/Lotterhos_Lab/VCF/", fileNumber, ".plots/")
+
+#TRUE POSITIVE
+#########################################################################################################
+library(ggplot2)
+TP.table$expectedAdditive<-abs(TP.table$expectedAdditive)
+TP.table$tasselAdditive<-abs(TP.table$tasselAdditive)
+
+truePositiveNumber<-length(TP.table$basePairPosition)
+
+x.min.add<-min(TP.table$expectedAdditive)
+x.max.add<-max(TP.table$expectedAdditive)
+y.min.add<-min(TP.table$tasselAdditive)
+y.max.add<-max(TP.table$tasselAdditive)
+
+if(x.min.add < y.min.add){
+  min.add<-x.min.add
+}else{
+  min.add<-y.min.add
+}
+
+if(x.max.add > y.max.add){
+  max.add<-x.max.add
+}else{
+  max.add<-y.max.add
+}
+
+min.add<-min.add - 0.25
+max.add<-max.add + 0.25
+
+ggplot(data = TP.table) +
+  geom_point(mapping = aes(x = expectedAdditive, y = tasselAdditive)) +
+  xlab("Expected Additive") + ylab("Observed Additive") +
+  ggtitle("Observed vs Expected Additive Effects for Significant and Causal Loci") +
+  geom_abline(mapping = NULL, slope = 1, yintercept = 0) +
+  theme(aspect.ratio = 1) +
+  coord_cartesian(xlim = c(min.add, max.add), ylim = c(min.add, max.add), expand = FALSE) +
+  theme(plot.title = element_text(hjust = 0.4))
+
+outfile<-paste0(fileNumber, "_TP.EvsO.add.png")
+ggsave(outfile, last_plot(), path = path)
+unlink(outfile)
+
+x.min.dom<-min(TP.table$expectedDominance)
+x.max.dom<-max(TP.table$expectedDominance)
+y.min.dom<-min(TP.table$tasselDominance)
+y.max.dom<-max(TP.table$tasselDominance)
+
+if(x.min.dom < y.min.dom){
+  min.dom<-x.min.dom
+}else{
+  min.dom<-y.min.dom
+}
+
+if(x.max.dom > y.max.dom){
+  max.dom<-x.max.dom
+}else{
+  max.dom<-y.max.dom
+}
+
+min.dom<-min.dom - 0.15
+max.dom<-max.dom + 0.15
+
+ggplot(data = TP.table) +
+  geom_point(mapping = aes(x = expectedDominance, y = tasselDominance)) +
+  xlab("Expected Dominance") + ylab("Observed Dominance") +
+  ggtitle("Observed vs Expected Dominance Effects for Significant and Causal Loci") +
+  geom_abline(mapping = NULL, slope = 1, yintercept = 0) +
+  theme(aspect.ratio = 1) +
+  coord_cartesian(xlim = c(min.dom, max.dom), ylim = c(min.dom, max.dom), expand = FALSE) +
+  theme(plot.title = element_text(hjust = 0.4))
+
+outfile<-paste0(fileNumber, "_TP.EvsO.dom.png")
+ggsave(outfile, plot = last_plot(), path = path)
+unlink(outfile)
+
+
+#FALSE NEGATIVE
+########################################################################################################
+
+FN.table$expectedAdditive<-abs(FN.table$expectedAdditive)
+FN.table$tasselAdditive<-abs(FN.table$tasselAdditive)
+
+falseNegativeNumber<-length(FN.table$basePairPosition)
+
+if(falseNegativeNumber > 0){
+
+  x.min.add<-min(FN.table$expectedAdditive)
+  x.max.add<-max(FN.table$expectedAdditive)
+  y.min.add<-min(FN.table$tasselAdditive)
+  y.max.add<-max(FN.table$tasselAdditive)
+
+  if(x.min.add < y.min.add){
+    min.add<-x.min.add
+  }else{
+    min.add<-y.min.add
+  }
+
+  if(x.max.add > y.max.add){
+    max.add<-x.max.add
+  }else{
+    max.add<-y.max.add
+  }
+
+  min.add<-min.add - 0.25
+  max.add<-max.add + 0.25
+
+  ggplot(data = FN.table) +
+    geom_point(mapping = aes(x = expectedAdditive, y = tasselAdditive)) +
+    xlab("Expected Additive") + ylab("Observed Additive") +
+    ggtitle("Observed vs Expected Additive Effects for Nonsignificant and Causal Loci") +
+    geom_abline(mapping = NULL, slope = 1, yintercept = 0) +
+    theme(aspect.ratio = 1) +
+    coord_cartesian(xlim = c(min.add, max.add), ylim = c(min.add, max.add), expand = FALSE) +
+    theme(plot.title = element_text(hjust = 0.4))
+
+  outfile<-paste0(fileNumber, "_FN.EvsO.add.png")
+  ggsave(outfile, last_plot(), path = path)
+  unlink(outfile)
+
+  x.min.dom<-min(FN.table$expectedDominance)
+  x.max.dom<-max(FN.table$expectedDominance)
+  y.min.dom<-min(FN.table$tasselDominance)
+  y.max.dom<-max(FN.table$tasselDominance)
+
+  if(x.min.dom < y.min.dom){
+    min.dom<-x.min.dom
+  }else{
+    min.dom<-y.min.dom
+  }
+
+  if(x.max.dom > y.max.dom){
+    max.dom<-x.max.dom
+  }else{
+    max.dom<-y.max.dom
+  }
+
+  min.dom<-min.dom - 0.15
+  max.dom<-max.dom + 0.15
+
+  ggplot(data = FN.table) +
+    geom_point(mapping = aes(x = expectedDominance, y = tasselDominance)) +
+    xlab("Expected Dominance") + ylab("Observed Dominance") +
+    ggtitle("Observed vs Expected Dominance Effects for Nonsignificant and Causal Loci") +
+    geom_abline(mapping = NULL, slope = 1, yintercept = 0) +
+    theme(aspect.ratio = 1) +
+    coord_cartesian(xlim = c(min.dom, max.dom), ylim = c(min.dom, max.dom), expand = FALSE) +
+    theme(plot.title = element_text(hjust = 0.4))
+  
+
+  outfile<-paste0(fileNumber, "_FN.EvsO.dom.png")
+  ggsave(outfile, last_plot(), path = path)
+  unlink(outfile)
+}
+#FALSE POSITIVE
+########################################################################################################
+
+FP.table$tasselAdditive<-abs(FP.table$tasselAdditive)
+FP.table$expectedAdditive<-0
+
+falsePositiveNumber<-length(FP.table$basePairPosition)
+
+x.min.add<-min(FP.table$expectedAdditive)
+x.max.add<-max(FP.table$expectedAdditive)
+y.min.add<-min(FP.table$tasselAdditive)
+y.max.add<-max(FP.table$tasselAdditive)
+
+if(x.min.add < y.min.add){
+  min.add<-x.min.add
+}else{
+  min.add<-y.min.add
+}
+
+if(x.max.add > y.max.add){
+  max.add<-x.max.add
+}else{
+  max.add<-y.max.add
+}
+
+min.add<-min.add - 0.25
+max.add<-max.add + 0.25
+
+ggplot(data = FP.table) +
+  geom_point(mapping = aes(x = expectedAdditive, y = tasselAdditive)) +
+  xlab("Expected Additive") + ylab("Observed Additive") +
+  ggtitle("Observed vs Expected Additive Effects for Significant and Neutral Loci") +
+  geom_abline(mapping = NULL, slope = 1, yintercept = 0) +
+  theme(aspect.ratio = 1) +
+  coord_cartesian(xlim = c(min.add, max.add), ylim = c(min.add, max.add), expand = TRUE) +
+  theme(plot.title = element_text(hjust = 0.4))
+
+outfile<-paste0(fileNumber, "_FP.EvsO.add.png")
+ggsave(outfile, last_plot(), path = path)
+unlink(outfile)
+
+x.min.dom<-min(FP.table$expectedDominance)
+x.max.dom<-max(FP.table$expectedDominance)
+y.min.dom<-min(FP.table$tasselDominance)
+y.max.dom<-max(FP.table$tasselDominance)
+
+if(x.min.dom < y.min.dom){
+  min.dom<-x.min.dom
+}else{
+  min.dom<-y.min.dom
+}
+
+if(x.max.dom > y.max.dom){
+  max.dom<-x.max.dom
+}else{
+  max.dom<-y.max.dom
+}
+
+min.dom<-min.dom - 0.15
+max.dom<-max.dom + 0.15
+
+ggplot(data = FP.table) + 
+  geom_point(mapping = aes(x = expectedDominance, y = tasselDominance)) +
+  xlab("Expected Dominance") + ylab("Observed Dominance") +
+  ggtitle("Observed vs Expected Dominance Effects for Significant and Neutral Loci") +
+  geom_abline(mapping = NULL, slope = 1, yintercept = 0) +
+  coord_cartesian(xlim = c(min.dom, max.dom), ylim = c(min.dom, max.dom), expand = TRUE) +
+  theme(plot.title = element_text(hjust = 0.4))
+
+outfile<-paste0(fileNumber, "_FP.EvsO.dom.png")
+ggsave(outfile, last_plot(), path = path)
+unlink(outfile)
+print("Made it this far")
+
+#Store confusion matrix data in a table
+########################################################################################################
+
+trueNegativeNumber<-length(TN.table$basePairPosition)
+falseNegativeMissingNumber<-length(FN.missing.table$basePairPosition)
+
+CM.lines<-paste(truePositiveNumber, falseNegativeNumber, falsePositiveNumber, trueNegativeNumber, falseNegativeMissingNumber, sep = "\t")
+fileConn<-file("~/Desktop/Lotterhos_Lab/allPlots/confusion_matrix.txt")
+writeLines(CM.lines, fileConn)
+
+message <-paste0("Done with ", fileNumber)
+print(message)
+
+}#End of Initial For Loop to Iterate Over Datasets
+
+close(fileConn)
---
+++ FerolitoScripts/TASSEL/domAdd.pl
@@ -0,0 +1,272 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+use feature qw(say);
+use Getopt::Long;
+use Pod::Usage;
+use List::MoreUtils qw(zip);
+use Data::Dumper qw(Dumper);
+
+########################################################################
+# 
+# File   :  domAdd.pl
+# History:  This program was written by Brian Ferolito   
+#
+########################################################################
+#
+# This program takes a MLM output file from TASSEL and a scanResults.txt
+# file from the command line. Both files are read into arrays whith each
+# line as an element.
+#
+########################################################################
+
+my $scanFile;
+my $sigTasselFile;
+my $nonSigTasselFile;
+my $version;
+my $usage = "\n$0 [options] \n
+
+Options:
+
+-scanFile			File to open (Scan Results file Ex. 10900_Invers_ScanResults.txt)
+-sigTasselFile		File to open (Significant Tassel MLM Ex. 10900.MLM.significant.txt)
+-nonSigTasselFile	File to open (Non Significant Tassel MLM Ex. 10900.MLM.not_significant.txt)
+-version			Dataset Number (10900)
+-help				Show this message
+
+";
+
+GetOptions(
+
+	'scanFile=s'		 =>\$scanFile,
+	'sigTasselFile=s'	 =>\$sigTasselFile,
+	'nonSigTasselFile=s' =>\$nonSigTasselFile,
+	'version=s'			 =>\$version,
+	help			     => sub {pod2usage($usage); },
+	
+	) or die($usage);
+
+unless ($scanFile){
+	die "Provide a file name to open, -scanFile <*_Invers_ScanResults.txt>\n " , $usage;
+}	
+
+unless ($sigTasselFile){
+	die "Provide a file name to open, -sigTasselFile <*.MLM.significant.txt>\n " , $usage;
+}
+
+unless ($nonSigTasselFile){
+	die "Provide a file name to open, -tasselFile <*.MLM.not_significant.txt>\n " , $usage;
+}
+
+unless ($version){
+	die "Provide a version, -version <10900>\n ", $usage;
+}	
+
+########################################################################
+#Outfiles
+
+my $tp_out = join('', $version, '.domAdd.TP.test.txt');
+my $fn_out = join('', $version, '.domAdd.FN.test.txt');
+my $fp_out = join('', $version, '.domAdd.FP.test.txt');
+my $tn_out = join('', $version, '.domAdd.TN.test.txt');
+
+my $tpFh   = getFh('>', $tp_out);
+my $fnFh   = getFh('>', $fn_out);
+my $fpFh   = getFh('>', $fp_out);
+my $tnFh   = getFh('>', $tn_out);
+
+########################################################################
+
+my $scanFh    							= getFh('<', $scanFile);
+my $sigTasselFh  						= getFh('<', $sigTasselFile);
+my $nonSigTasselFh						= getFh('<', $nonSigTasselFile);
+my ($causalScanData, $neutralScanData)  = getScanData2Array($scanFh);
+
+my @causalScanData  = @$causalScanData;
+my @neutralScanData = @$neutralScanData;
+
+my @causalBpArray   = getBParray(@causalScanData);
+my @neutralBpArray	= getBParray(@neutralScanData);
+
+
+my @tp_matchedData 	= matchTassel2Scan($sigTasselFh,    \@causalScanData,  \@causalBpArray);
+my @fn_matchedData	= matchTassel2Scan($nonSigTasselFh, \@causalScanData,  \@causalBpArray);
+my @fp_matchedData	= matchTassel2Scan($sigTasselFh,    \@neutralScanData, \@neutralBpArray);
+my @tn_matchedData	= matchTassel2Scan($nonSigTasselFh, \@neutralScanData, \@neutralBpArray);
+
+
+my @tp_finalArray   = processFinalArray(@tp_matchedData);
+my @fn_finalArray	= processFinalArray(@fn_matchedData);
+my @fp_finalArray	= processFinalArray(@fp_matchedData);
+my @tn_finalArray	= processFinalArray(@tn_matchedData);
+
+
+print2File($tpFh, \@tp_finalArray);
+print2File($fnFh, \@fn_finalArray);
+print2File($fpFh, \@fp_finalArray);
+print2File($tnFh, \@tn_finalArray);
+
+
+
+########################################################################
+# The following subroutine that returns the bp position, additive, and 
+# dominance effect of the Tassel output file
+
+sub _tassel {
+	my ($tasselFh) = @_;
+	my @effectsStore;
+	while(<$tasselFh>){
+		chomp $_;
+		my $char = substr($_, 0, 5);
+		if ( ! ($char eq "Trait")){
+			my @line = split(" ", $_);
+			my $domEffect = $line[10];
+			my $addEffect = $line[7];
+			my $pos = $line[3];
+			
+			my @effects = join("\t", $pos, $addEffect, $domEffect);
+			push (@effectsStore, @effects)
+		}
+	}
+	return @effectsStore;
+}
+
+###################################################################################
+# The following subroutine takes two arguments. The first is whether it is a read or
+# write operator. The second is the file name to be either opened or written to. A
+# file handle is returned
+
+sub getFh {
+	my ($readOrwrite, $file) = @_;
+	if($readOrwrite eq '<'){
+		my $inFh;
+		unless (open($inFh, '<', $file)){
+			die "Unable to open $file for reading", $!;
+		}
+		return $inFh;
+	}
+	elsif($readOrwrite eq '>'){
+		my $outFh;
+		unless(open($outFh, '>', $file)){
+			die "Unable to open $file for writing", $!;
+		} 
+		return $outFh;
+	}
+	else{ 
+		die "This is not a read or write operator";
+	}
+}
+
+###################################################################################
+
+sub getScanData2Array{
+	my ($scanFh) = @_;
+	my @causalScanData;
+	my @neutralScanData;
+	while(<$scanFh>){
+		chomp $_;
+		my @line = split(" ", $_);
+		my $position = $line[1];
+		my $muttype = $line[4];
+		$muttype =~ tr/"//d; #eliminate quotation marks to make life easier later
+		my $selfCoef = $line[30];
+		my $dom = 0;
+	
+		if($muttype eq "MT=2"){
+			my @causalTabLine = join("\t", $position, $muttype, $selfCoef, $dom);
+			push (@causalScanData, @causalTabLine);
+		}elsif($muttype eq "MT=1"){
+			my @neutralTabLine = join("\t", $position, $muttype, $selfCoef, $dom);
+			push (@neutralScanData, @neutralTabLine);
+		}else{
+			next;
+		}
+	}
+	return (\@causalScanData, \@neutralScanData);
+}
+
+###################################################################################
+
+sub getBParray{
+	my @scanData = @_;
+	my @bpArray;
+	foreach(@scanData){
+		chomp $_;
+		my @line = split(" ", $_);
+		push (@bpArray, $line[0])
+	}
+	return @bpArray;
+}
+
+###################################################################################
+
+#Matches the base pair positions of both data sets, both observed and expected.
+#Adds the expected additive effect and the expected dominance effect to the observed 
+#data if there is a match.
+
+sub matchTassel2Scan{
+	
+	my ($tasselFh, $scanDataRef, $bpArrayRef) = @_;
+	my $length_bpArray = scalar @$bpArrayRef;
+	my @scanData       = @$scanDataRef;
+	my @bpArray        = @$bpArrayRef;
+	my @tasselData = _tassel($tasselFh);
+	
+	
+	for(my $i = 0; $i <= $length_bpArray-1; $i++){
+		foreach(@tasselData){
+			chomp $_;
+			my @line = split(" ", $_);
+			if($line[0] eq $bpArray[$i]){
+						
+				my $finalLine = $scanData[$i];
+				my @finalLineArray = split(" ", $finalLine);
+			
+				if(! ($line[1] eq 'NaN' | $line[2] eq 'NaN')){
+					push (@finalLineArray, $line[1]);
+					push (@finalLineArray, $line[2]);
+					$finalLine = join("\t", @finalLineArray);
+					$scanData[$i] = $finalLine;
+				}
+			}
+		}
+	}
+	return @scanData;
+}
+
+###################################################################################
+
+sub processFinalArray{
+	my @matchedData = @_;
+	my @finalArray;
+	foreach(@matchedData){
+		chomp $_;
+		my @line = split(" ", $_);
+		my $lineNumber = scalar @line;
+		if($lineNumber eq 6){
+			my $tabLine = join("\t", @line);
+			push (@finalArray, $tabLine);
+		}
+	}
+	return @finalArray;
+}
+
+###################################################################################
+
+sub print2File{
+	my ($outFh, $finalArrayRef) = @_;
+	say $outFh join("\t", "basePairPosition", "expectedAdditive", "expectedDominance", "tasselAdditive", "tasselDominance");
+
+	foreach (@$finalArrayRef){
+		chomp $_;
+		my @line = split(" ", $_);
+		my $MTremoved = join("\t", $line[0], $line[2], $line[3], $line[4], $line[5]);
+		say $outFh $MTremoved;
+	}
+}
+
+###################################################################################
+
+close $sigTasselFh;
+close $nonSigTasselFh;
+close $scanFh;
