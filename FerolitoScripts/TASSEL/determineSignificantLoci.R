40a327e3c50f8ca7b443f0a1cff845cb1727bbbb
BrianFerolito <brian_ferolito@outlook.com>
2018-11-26 18:55:32 -0500

    Added comments

--- FerolitoScripts/TASSEL/determineSignificantLoci.R
+++ FerolitoScripts/TASSEL/determineSignificantLoci.R
@@ -5,22 +5,29 @@ library("ggplot2")
 
 setwd("~/Desktop/Lotterhos_Lab/VCF")
 
+#Command Line options
 args <- commandArgs(trailingOnly = TRUE)
 fn <- args[1]
 dataNumber <- args[2]
+
+#Read file into R as df
 tassel_data<-read.table(fn, header = T, sep = "\t")
 
+#Benjamini Hochberg adjustment
 q<-p.adjust(tassel_data$p, method = "BH")
 tassel_data$Q<-q
 
+#Significant loci stored in a df
 tassel_data_significant<-tassel_data %>%
   filter(Q < 0.05) %>%
   filter(add_effect != "NaN")
 
+#Non significant loci stored in a df
 tassel_data_not_significant<-tassel_data %>%
   filter(Q > 0.05) %>%
   filter(add_effect != "NaN")
 
+#Write data frames to separate outfiles
 significant_outfile<-paste0("~/Desktop/Lotterhos_Lab/VCF/",dataNumber,".MLM.significant.txt")
 not_significant_outfile<-paste0("~/Desktop/Lotterhos_Lab/VCF/",dataNumber,".MLM.not_significant.txt")
 
